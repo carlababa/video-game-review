ce42732d2a3d539c1ae271defff01db7f03c6f73
“Brian <brianlens@gmail.com>
2016-04-28 17:52:49 +0200

    WILL IT WORK NOW HUH?

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,21 +10,45 @@ class GamesController < ApplicationController
   end
 
   def show
-    @games = @category.games.find(params[:id])
+    @game = @category.game.find(params[:id])
+    render json: { category: @category, game: @game }
   end
 
+  def create
+    @game = Game.new
+    if @game.save
+      render json: { category: @category, games: @games, location: game_url(@game)}, status: 201
+    else
+      render json: { errors: @game.errors }, status:422
+    end
+  end
+
+  def update
+    @game = @category.game.find(params[:id])
+      if @category.game(.update(game_params))
+      render json: {category: @category, games: @games}
+    else
+      render json: {errors:@game.errors}
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    if @game.destroy
+      render json: {game: nil}
+    else
+      render json: {errors@game.errors}
+    end
+  end
+
+
   protected
+  def game_params
+    params.require(:game).permit(:title, :cover_image)
+  end
+
 
   def set_category
     @category = Category.find(params[:category_id])
   end
-
-  # def create
-  #   @game = Game.new(game_params)
-  #   @game.category = @category
-  #
-  #   if @game.save
-  #     redirect_to category_game_path(@category, @game)
-  #
-  # end
 end
