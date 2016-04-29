f6afee28e75c7bf227926784268fc37d52326974
Carla Babadopulos <cacababa@gmail.com>
2016-04-29 16:33:22 +0200

    changed port for localhost:4000

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 class VideogameReviews::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
-      origins 'localhost:3000', '127.0.0.1:3000'
+      origins 'localhost:4000', '127.0.0.1:4000'
 
       resource '/cors',
         headers: :any,
